15d617674fadc56adb18bc5169fe4850b672aac8
Thiago Nogueira <nogueira.tcs@gmail.com>
2020-09-16 02:36:05 -0300

    :sparkles: desafio07 finalizado

---
+++ src/utils/formatValue.ts
@@ -0,0 +1,7 @@
+const formatValue = (value: number): string =>
+  Intl.NumberFormat('pt-br', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value); // TODO
+
+export default formatValue;
